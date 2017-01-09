6c2a87b336c5b6010886e79c439a6405b41439f4
mathieudaviet <mathieu.daviet@gmx.com>
2017-01-08 22:09:02 -0500

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,12 @@
 # config valid only for current version of Capistrano
 lock "3.7.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "skiller"
+set :repo_url, "git@github.com:mathieudaviet/skillers.git"
+set :deploy_to, '/home/deploy/skillers'
+
+append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
